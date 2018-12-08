fab9e9ffa19a5bf2eebc25c66af02185b4772120
Taylor Swinton <taylorswinton1@gmail.com>
2018-12-08 20:30:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,40 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log('hello'.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+
+  if (lowercase === 'hello') {
+    return("I can't hear you!")
+  }
+  
+  if (uppercase === 'HELLO') {
+    return("YES INDEED!")
+  }
+}
+
+/*function sayHiToGrandma(string){
+  var lowercase = 'hello'
+  var uppercase = 'HELLO'
+  var mixedCase = 'I love you, Grandma.'
+  
+  if (lowercase === "hello") {
+    return("I can't hear you!")
+  }
+  
+  if (uppercase === "HELLO") {
+    return("YES INDEED!")
+  }
+  
+  if (mixedCase === "I love you, Grandma.") {
+    return("I love you, too.")
+  }
+}*/
