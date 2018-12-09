93210327d703d47f490d395809221ec516a420ff
Taylor Swinton <taylorswinton1@gmail.com>
2018-12-09 01:27:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log('hello'.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if ('string'.toLowerCase()){
+    return ("I can't hear you!")
+  } else ('string'.toUpperCase()){
+    return ("YES INDEED!")
+  } else ('string' === "I love you, Grandma."){
+    return ("I love you, too.")
+  }
+}
